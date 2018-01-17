9bedbf6f9908de141831c136e8dcd2aed1f17b59
loovlens <loovlens@gmail.com>
2018-01-17 13:00:15 +0100

    added comments

--- src/pl/alphabyte/oilSimulator/CurrentCalculator.java
+++ src/pl/alphabyte/oilSimulator/CurrentCalculator.java
@@ -1,6 +1,21 @@
 package pl.alphabyte.oilSimulator;
+
+/**
+ *
+ */
+
 public class CurrentCalculator {
 
+    /**
+     * calculates line equations for the line constructed through 2 points
+     * and two perpendicular lines constructed through each of the points
+     *
+     * @param x1 the x coordinate of the first point
+     * @param x2 the y coordinate of the first point
+     * @param y1 the x coordinate of the second point
+     * @param y2 the y coordinate of the second point
+     * @return array contains 3line equations factors: main line in the first row, both perpendicular lines in the second
+     */
     public static double[][] calculateLinesEquationsTab(double x1, double x2, double y1, double y2){
         double[][] paramsTab = new double[2][3]; // tablica wspolczynników 3 prostych
         //wiersz 1 zawiera wspolczynniki A B C prostej wyznaczonej przez wektor w postaci Ax + By + C = 0
@@ -52,10 +67,16 @@ public class CurrentCalculator {
         return paramsTab;
     }
 
+
+    /**
+     * calculates normalized vector based on two points
+     * @param x1 the x coordinate of the first point
+     * @param x2 the y coordinate of the first point
+     * @param y1 the x coordinate of the second point
+     * @param y2 the y coordinate of the second point
+     * @return 2D-vector in a array
+     */
     public static double[] calculateVector(double x1, double x2, double y1, double y2){
-
-        // normalizacja wektora do przedziału <0, 1>
-
         double [] vector = new double[2];
         double lenght;
 
@@ -69,6 +90,14 @@ public class CurrentCalculator {
 
         return vector;
     }
+
+    /**
+     * calculates distance of a point (i,j) from the line
+     * @param i the x coordinate of the point
+     * @param j the y coordinate of the point
+     * @param linesParamsTab array contains 3line equations factors: main line in the first row, both perpendicular lines in the second
+     * @return calculated distance
+     */
     public static double calculateDistance(int i, int j, double[][] linesParamsTab){
         //obliczanie odleglosci punktu (i,j) do prostej o parametrach w tablicy
         double dist;
@@ -77,6 +106,15 @@ public class CurrentCalculator {
         return dist;
     }
 
+    /**
+     * test if the point (i,j) is located within certain distance from the line constrained by two perpendicular lines
+     * @param dist max distance from the main line
+     * @param linesParamsTab array contains 3line equations factors: main line in the first row, both perpendicular lines in the second
+     * @param i the x coordinate of the point
+     * @param j the y coordinate of the point
+     * @param vector 2D-vector in a array
+     * @return true if the point is located within said area
+     */
     public static boolean isPointNearCurrent(double dist, double[][] linesParamsTab, int i, int j, double[] vector){
         //testowanie czy dany punkt znajduje się w sąsiedztwie wektora
         boolean val;
